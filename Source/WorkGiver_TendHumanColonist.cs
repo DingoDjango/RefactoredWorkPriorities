ac85c925e5ccd9e7f59d6955b8dc2d5a2fa018c7
Dingo <oferdottan@gmail.com>
2016-10-23 09:28:27 +0300

    RWP 1.3
    Doctor priorities are back:
    
    * Doctors will rescue colonists before rescuing other pawns (e.g. animals). 
    * Doctors will tend to colonist injuries before tending to animals & outsiders.

---
+++ Source/WorkGiver_TendHumanColonist.cs
@@ -0,0 +1,19 @@
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace RWP
+{
+	public class WorkGiver_TendHumanColonist : WorkGiver_Tend
+	{
+        public override bool HasJobOnThing(Pawn pawn, Thing t)
+        {
+            Pawn pawn2 = t as Pawn;
+            if (!pawn2.IsColonist)
+            {
+                return false;
+            }
+            return pawn2 != null && pawn2 != pawn && pawn2.InBed() && pawn2.health.ShouldBeTendedNow && pawn.CanReserve(pawn2, 1);
+        }
+	}
+}
