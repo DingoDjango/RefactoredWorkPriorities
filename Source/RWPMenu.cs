65b3a0bc6baf5de057d00093a14a399eb6e5139e
Dingo <oferdottan@gmail.com>
2016-12-22 16:21:20 +0200

    Repair Expansion
    Users may now set a custom value in the menu for repair threshold. If
    the building's HP as a percentage is above this threshold, pawns will
    not repair it.
    
    E.g. wooden wall with 100/140 HP has ~71% HP and will only be repaired
    if the threshold is >71.

--- Source/RWPMenu.cs
+++ Source/RWPMenu.cs
@@ -1,4 +1,5 @@
 ﻿using HugsLib;
+using HugsLib.Settings;
 using Verse;
 
 namespace RWP
@@ -27,6 +28,9 @@ namespace RWP
 		{
 			var prioritizeTreatingColonists = Settings.GetHandle<bool>("RWP_Doctor_PrioritizeTreatingColonists", "RWP_setting_doctorTreatColonist_label".Translate(), "RWP_setting_doctorTreatColonist_desc".Translate(), true);
 			WorkGiver_TendPrioritized.TendColonistsFirst = prioritizeTreatingColonists.Value;
+
+			var repairThresholdInt = Settings.GetHandle<int>("RWP_Repair_RepairThresholdInt", "RWP_setting_repairThresholdInt_label".Translate(), "RWP_setting_repairThresholdInt_desc".Translate(), 100, Validators.IntRangeValidator(0, 100));
+			WorkGiver_RepairCustom.RepairThreshold = repairThresholdInt.Value;
 
 			var haulRottables = Settings.GetHandle<bool>("HaulRottables", "RWP_setting_haulRottables_label".Translate(), "RWP_setting_haulRottables_desc".Translate(), true);
 			WorkGiver_HaulRottable.PrioritizeRottable = haulRottables.Value;
---
+++ Source/WorkGiver_RepairCustom.cs
@@ -0,0 +1,17 @@
+﻿using RimWorld;
+using Verse;
+
+namespace RWP
+{
+	public class WorkGiver_RepairCustom : WorkGiver_Repair
+	{
+		public static int RepairThreshold = 100;
+
+		public override bool HasJobOnThing(Pawn pawn, Thing t)
+		{
+			int hitPointPercentage = 100 * t.HitPoints / t.MaxHitPoints;
+
+			return base.HasJobOnThing(pawn, t) && hitPointPercentage < RepairThreshold;
+		}
+	}
+}
